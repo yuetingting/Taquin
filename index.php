f664b0f689419b6c61edfdf2c6f8e72cd1e7cc82
yuetingting <49209944+yuetingting@users.noreply.github.com>
2019-04-03 19:57:02 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <title>Taquin</title>
+		<meta name="author" content="YUE & TINGTING">
+    	<link type="text/css" rel="stylesheet" href="Taquin.css" />
+        <script type="text/javascript" src="Taquin.js"></script>
+  </head>
+
+<body>
+	<h1>Taquin</h1>
+
+	<h2 style="visibility:visible">Choisissez la taille de votre taquin !</h2>
+
+	<h3>
+	<button style="visibility:visible">StartGame</button>
+	<button style="visibility:hidden">2*2</button>
+	<button style="visibility:hidden">2*3</button>
+	<button style="visibility:hidden">3*2</button>
+	<button style="visibility:hidden">3*3</button>
+	</h3>
+
+
+	<div id="result" style="visibility:hidden">Bravo vous avez terminé le taquin !!</div>
+	<div id="fois" style="visibility:hidden">fois</div>
+	<button style="visibility:hidden">RESTART</button>
+
+<div id="taquin">
+	</div>
+
+
+
+</body>
+</html>
